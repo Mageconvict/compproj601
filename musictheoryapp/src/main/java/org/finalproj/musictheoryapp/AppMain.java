677e9c092abe90f1bdc8c31bbb36593d285191c6
Ben Highgate <91388545+Mageconvict@users.noreply.github.com>
2024-05-05 19:56:52 +0100

    Drawing the Bar
    Created the bar staff lines to draw in BarModel

--- musictheoryapp/src/main/java/org/finalproj/musictheoryapp/AppMain.java
+++ musictheoryapp/src/main/java/org/finalproj/musictheoryapp/AppMain.java
@@ -2,6 +2,7 @@ package org.finalproj.musictheoryapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,15 +11,16 @@ import java.io.IOException;
 public class AppMain extends Application {
 
     SystemModel sysmod = SystemModel.getInstance();
+    BarModel barModel = new BarModel();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), sysmod.getScreenWidth(), sysmod.getScreenHeight());
+        Scene scene = new Scene(barModel.getBarModel(), sysmod.getScreenWidth(), sysmod.getScreenHeight());
         stage.setTitle("Music Theory Application");
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
-
     }
 
     public static void main(String[] args) {
--- musictheoryapp/src/main/java/org/finalproj/musictheoryapp/BarModel.java
+++ musictheoryapp/src/main/java/org/finalproj/musictheoryapp/BarModel.java
@@ -7,20 +7,20 @@ import javafx.scene.shape.Line;
 import java.util.ArrayList;
 
 public class BarModel {
-    ArrayList<Line> arrLines;
+    Line line1 = new Line(200, 200, 1760, 200);
+    Line line2 = new Line(200, 220, 1760, 220);
+    Line line3 = new Line(200, 240, 1760, 240);
+    Line line4 = new Line(200, 260, 1760, 260);
+    Line line5 = new Line(200, 280, 1760, 280);
+    Line line6 = new Line(200, 300, 1760, 300);
 
-    Line line1 = new Line(200, 200, 880, 200);
-    Line line2 = new Line(200, 220, 880, 220);
-    Line line3 = new Line(200, 240, 880, 240);
-    Line line4 = new Line(200, 260, 880, 260);
-    Line line5 = new Line(200, 280, 880, 280);
-    Line line6 = new Line(200, 300, 880, 300);
+    Group root = new Group(line1, line2, line3, line4, line5, line6);
 
-    Group bar = new Group();
-    bar.getChildren().addAll(line1, line2, line3, line4, line5, line6);
+    public BarModel() {
+    }
 
-    public BarModel(int bars) {
-
+    public Group getBarModel(){
+        return root;
     }
 
 }
